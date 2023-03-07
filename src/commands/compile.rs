b3534ff02a5986489dd6cb722a954caa1c8d1782
Thomas Byr <tbouyer2000@gmail.com>
2023-03-07 12:29:11 +0100

    ensure locks are closed as soon as possible

--- src/commands/compile.rs
+++ src/commands/compile.rs
@@ -121,13 +121,19 @@ pub async fn handle_request(
   let _ = discordhelpers::delete_bot_reacts(&ctx, msg, loading_reaction).await;
 
   let is_success = is_success_embed(&result.1);
+  {
+    // stats manager is used in events.rs, lets keep our locks very short
+    let stats = data_read.get::<StatsManagerCache>().unwrap().lock().await;
+    if stats.should_track() {
+      stats.compilation(&result.0.language, !is_success).await;
+    }
+  }
   let stats = data_read.get::<StatsManagerCache>().unwrap().lock().await;
   if stats.should_track() {
     stats.compilation(&result.0.language, !is_success).await;
   }
 
-  let data = ctx.data.read().await;
-  let config = data.get::<ConfigCache>().unwrap();
+  let config = data_read.get::<ConfigCache>().unwrap();
   let config_lock = config.read().await;
 
   if let Some(log) = config_lock.get("COMPILE_LOG") {
