b6eddfd9adec2e70e64bea6627d6503f8bf7139b
ThomasByr <tbouyer2000@gmail.com>
2023-11-08 10:35:16 +0100

    background runner [log]

--- run.bash
+++ run.bash
@@ -5,4 +5,5 @@ processes=$(pgrep -u "$USER" -f "discord-compiler-bot")
 xargs kill -9 <<< "$processes" > /dev/null 2>&1
 printf "\033[97mkilled processe(s):\033[0m\n%s\n\n" "$processes"
 
-( (./target/release/discord-compiler-bot) 2>&1 | tee bot.log )&
+nohup cargo run -r 2>&1 | tee bot.log &
+ 
